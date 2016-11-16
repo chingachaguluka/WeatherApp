084d93eff8eb1c7134043e6cc5613968d41a2dc6
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-11-16 22:20:37 +0200

    Redux promise and axios installed. Search form fixed

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -13,12 +13,18 @@ export default class SearchBar extends Component {
         this.setState({ term: event.target.value });
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
 
         return (
             <div>
-                <form className="input-group">
-                    <input type="text" onChange={this.onInputChange} value={this.props.term} placeholder="Enter the city to get weather info" />
+                <form className="input-group" onSubmit={this.onFormSubmit}>
+                    <input type="text" onChange={this.onInputChange} className="form-control"
+                    value={this.props.term} 
+                    placeholder="Enter the city to get weather info" />
                     <span className="input-group-btn">
                         <button type="submit" className="btn btn-secondary">Submit</button>
                     </span>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = '0f39e5392290578788165eeac5ae55bc';
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
